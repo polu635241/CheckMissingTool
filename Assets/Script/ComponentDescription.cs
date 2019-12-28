4c5bfc69426b6ede2ee749fa69b90c14a26e2821
polu635241@gmail.com <polu635241@gmail.com>
2019-12-28 17:46:31 +0800

    新增物件以及單一腳本的分析工具

---
+++ Assets/Script/ComponentDescription.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEditor;
+
+namespace CheckMissingTool.Model
+{
+	[Serializable]
+	public class ComponentDescription
+	{
+		[SerializeField]
+		ComponentStatus status;
+
+		[SerializeField]
+		List<string> missingFieldPaths = new List<string>();
+
+		[SerializeField]
+		string componentName;
+
+		public ComponentDescription(Component ownerComponent)
+		{
+			//GetComponents 抓的到 可是回傳null 表示整個腳本掉了
+			if (ownerComponent == null)
+			{
+				status = ComponentStatus.ScriptMissing;
+			}
+			else
+			{
+				componentName = ownerComponent.GetType ().ToString ();
+
+				SerializedObject componentSo = new SerializedObject (ownerComponent);
+
+				SerializedProperty fieldProp = componentSo.GetIterator();
+
+				while (fieldProp.Next(true))
+				{
+					if (fieldProp.propertyType == SerializedPropertyType.ObjectReference)
+					{
+						UnityEngine.Object _object = fieldProp.objectReferenceValue;
+
+						if (_object == null)
+						{
+							//InstanceID為0是 none 不為0 可是卻找不到對應的物件就是掉素材了
+							if (fieldProp.objectReferenceInstanceIDValue != 0)
+							{
+								status = ComponentStatus.FieldMissing;
+
+								string nativeFieldProp = fieldProp.propertyPath;
+
+								string processPath = ProcessNativePath(nativeFieldProp);
+
+								missingFieldPaths.Add (processPath);
+							}
+
+						}
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// 在unity的解釋器裡 gos[0] 會記錄成 gos.Array.data[0]
+		/// 這邊處裡成好閱讀版本
+		/// </summary>
+		/// <returns>The path.</returns>
+		/// <param name="input">Input.</param>
+		string ProcessNativePath(string input)
+		{
+			Regex regex = new Regex(".Array.data");
+			string result = regex.Replace(input, "");
+			return result;
+		}
+	}
+
+	public enum ComponentStatus
+	{
+		Safe,ScriptMissing,FieldMissing
+	}
+}
---
+++ Assets/Script/GoDescription.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CheckMissingTool.Model
+{
+	[Serializable]
+	class GoDescription
+	{
+		[NonSerialized]
+		GoDescription parent;
+
+		[SerializeField]
+		string path;
+
+		[SerializeField]
+		GameObject entity;
+
+		public GameObject Entity
+		{
+			get
+			{
+				return entity;
+			}
+		}
+		
+		public static List<GoDescription> RecursiveCollection (GameObject entity)
+		{
+			List<GoDescription> collections = new List<GoDescription> ();
+
+			Action<GoDescription> collectionCallback = (_goDescription) => 
+			{
+				collections.Add (_goDescription);
+			};
+
+			//透過建構式遞迴建構
+			GoDescription rootGoDescription = new GoDescription (entity, null);
+
+			collections.Add (rootGoDescription);
+			collections.AddRange (rootGoDescription.GetChildDescriptions ());
+
+			return collections;
+		}
+
+		GoDescription(GameObject entity, GoDescription parent)
+		{
+			this.entity = entity;
+
+			this.parent = parent;
+		}
+
+		List<GoDescription> GetChildDescriptions()
+		{
+			List<GoDescription> childDescriptions = new List<GoDescription> ();
+			
+			foreach (Transform child in this.Entity.transform) 
+			{
+				GoDescription childDescription = new GoDescription (child.gameObject, this);
+
+				childDescriptions.Add (childDescription);
+				childDescriptions.AddRange (childDescription.GetChildDescriptions ());
+			}
+
+			return childDescriptions;
+		}
+
+		public void Flush()
+		{
+			List<GameObject> parentsGos = new List<GameObject> ();
+
+			GoDescription focusDescription = parent;
+
+			//遞迴往上找 直到沒有上一層為止
+			while (true)
+			{
+				if (focusDescription != null) 
+				{
+					parentsGos.Add (focusDescription.Entity);
+
+					focusDescription = focusDescription.parent;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			//因為是一層一層往回找 所以為了方便閱讀要顛倒成Hierachy上的順序
+			parentsGos.Reverse ();
+
+			StringBuilder pathStringBuilder = new StringBuilder ();
+
+			parentsGos.ForEach (parentsGo=>
+				{
+					pathStringBuilder.Append($"{parentsGo.name}/");
+				});
+			pathStringBuilder.Append (this.Entity.name);
+
+			path = pathStringBuilder.ToString ();
+		}
+	}
+}
